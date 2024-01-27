3fa08e89532d41b27bd8f520e6ca33dcff5c68b7
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-01-27 17:25:07 +0200

    oeuvre unique added

--- client/mk-art/src/axiosUrl.jsx
+++ client/mk-art/src/axiosUrl.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // 192.168.43.231
 export default axios.create({
-    baseURL: 'http://localhost:5000/api/v1/',
+    baseURL: 'http://192.168.43.231:5000/api/v1/',
     headers: {
         "Content-Type" : "application/json"
     }
